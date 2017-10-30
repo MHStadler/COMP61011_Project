dffd04fce9dee1bc9d41bd16487b0f5551d55a2c
Michael Stadler <michael.stadler@postgrad.manchester.ac.uk>
2017-10-30 23:48:56 +0000

    Added testResult Files for fMeasure Training Routines

--- calcFMeasureForResult.m
+++ calcFMeasureForResult.m
@@ -2,11 +2,15 @@ function fMeasure = calcFMeasureForResult(res)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-Y = nnz(res.labels(:) == 1)
-P = nnz(res.truelabels(:) == 1)
-TP = nnz(res.truelabels(:) == 1 & res.labels(:) == 1)
+Y = nnz(res.labels(:) == 1);
+P = nnz(res.truelabels(:) == 1);
+TP = nnz(res.truelabels(:) == 1 & res.labels(:) == 1);
 
-fMeasure = 2 * (TP/P * TP/Y) / (TP/P + TP/Y);
-
+if(TP == 0)
+    fMeasure = 0;
+else
+    fMeasure = 2 * (TP/P * TP/Y) / (TP/P + TP/Y);
+end
+ 
 end
 
---
+++ fMeasureHeartTrainRoutine.m
@@ -0,0 +1,31 @@
+function [trainedModels, trainingResults, bestModel, testResult] = fMeasureHeartTrainRoutine()
+
+load samplers;
+
+[crossFold, test] = heartSampler.split(5, 5);
+
+crossFoldSampler = sampler(crossFold.data, crossFold.labels);
+
+minErrRate = 200;
+
+
+for i = 1:4
+    [tr, te] = crossFoldSampler.split(i, 4);
+    
+    model = decisionTree().withMinCount(10).train(tr.data, tr.labels);
+    res = model.test(te.data, te.labels);
+    
+    trainedModels(i, 1) = model;
+    trainingResults(i, 1) = res;
+
+    errRate = res.err();
+    
+    if(errRate < minErrRate)
+        minErrRate = errRate;
+        bestModel = model;
+    end
+end
+
+testResult = bestModel.test(test.data, test.labels);
+
+end
