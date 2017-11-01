eb2dba5bdb2423d53d0b27d549a44f0d9a52865b
Michael Stadler <michael.stadler@postgrad.manchester.ac.uk>
2017-11-01 17:32:07 +0000

    Changed rocCurve generation to plot multiple curves in one graph

--- rocCurve.m
+++ rocCurve.m
@@ -1,73 +1,92 @@
-function area = rocCurve(result)
+function area = rocCurve(results, labels, markers)
 
-probabilities = result.probs;
+resultCount = size(results, 2);
 
-resultSize = size(result.probs, 1);
+for j = 1:resultCount
+    result = results(j);
+    
+    probabilities = result.probs;
 
-for i=1:resultSize
-    probabilities(i, 2) = i;
+    resultSize = size(result.probs, 1);
+
+    for i=1:resultSize
+        probabilities(i, 2) = i;
+    end
+
+    P = nnz(result.truelabels(:) == 1);
+    N = nnz(result.truelabels(:) == 0);
+
+    TPPrev = 0;
+    FPPrev = 0;
+
+    TP = 0;
+    FP = 0;
+
+    prevVal = -1;
+
+    %2012a fix - sort by dimension 1, use - to get descending
+    sortedProbabilities = sortrows(probabilities, -1);
+
+    count = 0;
+
+    A = 0;
+    
+    xVals = [];
+    yVals = [];
+
+    for i=1:resultSize
+        val = sortedProbabilities(i, 1);
+
+        if(val ~= prevVal)
+            count = count + 1;
+
+            xVals(count, 1) = FP/N;
+            yVals(count, 1) = TP/P;
+
+            A = A + calcTrapezoidArea(FP, FPPrev, TP, TPPrev);
+
+            TPPrev = TP;
+            FPPrev = FP;
+
+            prevVal = val;
+        end
+
+        trueLabelIndex = sortedProbabilities(i, 2);
+        trueLabel = result.truelabels(trueLabelIndex, 1);
+
+        if(trueLabel == 1)
+            TP = TP + 1;
+        else
+            FP = FP + 1;
+        end
+    end
+
+    count = count + 1;
+
+    xVals(count, 1) = FP/N;
+    yVals(count, 1) = TP/P;
+
+    A = A + calcTrapezoidArea(N, FPPrev, P, TPPrev);
+
+    A = A / (N * P);
+
+    plot(xVals, yVals, 'marker', markers(j));
+    
+    hold all;
+    
+    area(j, 1) = A;
+    
+    ar{j} = labels(j);
 end
 
-P = nnz(result.truelabels(:) == 1);
-N = nnz(result.truelabels(:) == 0);
+grid;
 
-TPPrev = 0;
-FPPrev = 0;
-
-TP = 0;
-FP = 0;
-
-prevVal = -1;
-
-%2012a fix - sort by dimension 1, use - to get descending
-sortedProbabilities = sortrows(probabilities, -1);
-
-count = 0;
-
-A = 0;
-
-for i=1:resultSize
-    val = sortedProbabilities(i, 1);
-    
-    if(val ~= prevVal)
-        count = count + 1;
-        
-        xVals(count, 1) = FP/N;
-        yVals(count, 1) = TP/P;
-        
-        A = A + calcTrapezoidArea(FP, FPPrev, TP, TPPrev);
-        
-        TPPrev = TP;
-        FPPrev = FP;
-        
-        prevVal = val;
-    end
-    
-    trueLabelIndex = sortedProbabilities(i, 2);
-    trueLabel = result.truelabels(trueLabelIndex, 1);
-    
-    if(trueLabel == 1)
-        TP = TP + 1;
-    else
-        FP = FP + 1;
-    end
-end
-
-count = count + 1;
-
-xVals(count, 1) = FP/N;
-yVals(count, 1) = TP/P;
-
-A = A + calcTrapezoidArea(N, FPPrev, P, TPPrev);
-
-A = A / (N * P);
-
-plot(xVals, yVals, 'marker', 'x');
+hold off;
 
 xlabel('FP-rate');
 ylabel('TP-rate');
 
-area = A;
+legend(ar, 'Location', 'southeast');
 
 end
 
